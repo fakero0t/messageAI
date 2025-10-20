23cb3cee2cdee7801923f8ba266a4eb1ed32b366
Arial Gardner <arialgardner@gmail.com>
2025-10-20 12:39:14 -0400

    Initial Commit

---
+++ swift_demo/swift_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_demoApp.swift
+//  swift_demo
+//
+//  Created by ary on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
